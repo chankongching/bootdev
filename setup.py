32cad7484bad91e856199f786abf8d79c2b42793
root <root@ip-10-0-0-199.ap-southeast-1.compute.internal>
2016-03-07 14:47:03 +0000

    Inserting LICENSE and basic files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'bootdev',
+  packages = ['bootdev'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Bootdev command for AWS deployments',
+  author = 'chankongching',
+  author_email = 'chankongching@gmail.com',
+  url = 'https://github.com/chankongching/bootdev', # use the URL to the github repo
+  download_url = 'https://github.com/chankongching/bootdev/tarball/0.1', 
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
