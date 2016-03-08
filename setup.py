d61280b634bd043752794b6e890d2d4650767b94
root <root@ip-10-0-0-199.ap-southeast-1.compute.internal>
2016-03-08 09:25:38 +0000

    Update of bootdev command line

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
   name = 'bootdev',
   #packages = ['bootdev'], # this must be the same as the name above
-  version = '0.1.4',
+  version = '0.1.6',
   description = 'Bootdev command for AWS deployments',
   author = 'chankongching',
   author_email = 'chankongching@gmail.com',
@@ -12,7 +12,7 @@ setup(
   download_url = 'https://github.com/chankongching/bootdev/tarball/0.1', 
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   license='MIT',
-  scripts=['bin/aws'],
+  scripts=['bin/bootdev'],
   install_requires=[
     'boto3',
   ],
