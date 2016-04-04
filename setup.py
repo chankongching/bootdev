1424c5b615cbc0d9145bc2f16064dc4ee96ad201
root <root@ip-10-0-0-199.ap-southeast-1.compute.internal>
2016-04-04 13:56:05 +0000

    Commiting last changes

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+from distutils.core import Command
+from distutils.command.install import install as _install
 import os
 # Including all datafiles
 datadir = os.path.join('templates')
@@ -9,7 +11,7 @@ from distutils.core import setup
 setup(
   name = 'bootdev',
   #packages = ['bootdev'], # this must be the same as the name above
-  version = '0.2.3',
+  version = '0.2.9',
   description = 'Bootdev command for AWS deployments',
   author = 'chankongching',
   author_email = 'chankongching@gmail.com',
@@ -21,7 +23,7 @@ setup(
   include_package_data = True,
   data_files = datafiles,
   #data_files = [('', ['templates/*.template'])],
-  #package_data={'bootdev':['templates/*.template']},
+  package_data={'bootdev':['package.ini']},
   install_requires=[
     'boto3',
     'awscli'
@@ -51,3 +53,58 @@ setup(
     #'Programming Language :: Python :: 3.4',
   ],
 )
+
+from os.path import *
+import re
+class package_ini(Command):
+    """
+        Locate package.ini in all installed packages and patch it as requested
+        by wildcard references to install process.
+    """
+
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+
+    def visit(self, dirname, names):
+        packages = self.distribution.get_command_obj(build_py.__name__).packages
+        if basename(dirname) in packages:
+            if 'package.ini' in names:
+                self.patch(join(dirname, 'package.ini'))
+
+    def patch(self, ini_file):
+        print 'patching file' + ini_file
+        with open(ini_file,'r') as infile:
+            file_data = infile.readlines()
+        with open(ini_file,'w') as outfile:
+            for line in file_data:
+                _line = self.patch_line(line)
+                if _line:
+                    line = _line
+                outfile.write(line)
+
+    def patch_line(self, line):
+        """
+            Patch an installed package.ini with setup's variables
+        """
+        match = re.match('(?P<identifier>\w+)\s*=.*##SETUP_PATCH\\((?P<command>.*)\.(?P<variable>.*)\\)', line)
+        if not match:
+            return line
+        print 'Replacing:'+line
+        line = match.group('identifier')
+        line += ' = '
+        data = '(self).distribution.get_command_obj(\''+\
+                match.group('command')+'\')'+'.'+\
+                match.group('variable')
+        line += '\''+eval(data)+'\''
+        line += '\n'
+        print 'With:' + line
+        return line
+
+class install(_install):
+    from distutils.command.install import install as _install
+    sub_commands = _install.sub_commands + [
+        (package_ini.__name__, None)
+    ]
